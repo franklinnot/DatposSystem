07ee458706af35f68b58204cb9159a610d50e040
franklinnot <franklinnot@outlook.com>
2025-02-14 18:22:43 -0500

    login con funcion de relacion belong de usuario -> empresa

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -37,8 +37,7 @@ class AuthenticatedSessionController extends Controller
 
         // si se autentico el email y contraseña, verificamos los estados de empresa y usuario
         $usuario = Auth::user();
-        $empresa = Empresa::get_empresa($usuario->id_empresa);
-
+        $empresa = $usuario->empresa;
 
         if ($empresa->estado == 0) {
             // si la empresa esta inactiva
