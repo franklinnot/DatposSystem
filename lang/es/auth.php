bd318eceb99e195a5a15411f3108c01aeb3f5993
franklinnot <franklinnot@outlook.com>
2025-02-13 19:42:40 -0500

    implementacion de reporte de errores en español

---
+++ lang/es/auth.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'No hemos encontrado una cuenta que coincida con esos datos.',
+    'user_inactive' => 'Tu cuenta está inactiva.',
+    'empresa_inactive' => 'Tu empresa están inactiva.',
+    'password' => 'La contraseña es incorrecta.',
+    'throttle' => 'Haz realizado muchos intentos. Por favor, inténtalo nuevamente en :seconds segundos.',
+
+];
