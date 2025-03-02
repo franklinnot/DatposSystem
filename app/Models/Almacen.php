ee73b336d481d1e0df5ab923098c2587f725f3d2
franklinnot <franklinnot@outlook.com>
2025-03-02 00:33:17 -0500

    modelo de almacen

---
+++ app/Models/Almacen.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Almacen extends Model
+{
+    use HasFactory;
+
+    #region Setup del modelo
+    protected $table = 'almacen';
+    protected $primaryKey = 'id_almacen';
+    protected $fillable = [
+        'id_almacen',
+        'codigo',
+        'nombre',
+        'estado',
+        'id_empresa',
+        'ciudad',
+        'direccion'
+    ];
+    #endregion
+
+}
