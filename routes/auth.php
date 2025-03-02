aacbcd8441bbbfdc34683d86a858fef63982c031
franklinnot <franklinnot@outlook.com>
2025-03-01 23:32:54 -0500

    eliminacion de rutas no utilizadas

--- routes/auth.php
+++ routes/auth.php
@@ -33,44 +33,7 @@ Route::middleware(['auth', 'no.cache'])->group(function () {
     
 
     Route::post('register', [RegisteredUserController::class, 'store']);
-    
-    
-    // Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
-    //             ->name('password.request');
-    
-
-    // Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])
-    //             ->name('password.email');
-    
-
-    // Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])
-    //             ->name('password.reset');
-    
-
-    // Route::post('reset-password', [NewPasswordController::class, 'store'])
-    //             ->name('password.store');
-
-
-    // Route::get('verify-email', EmailVerificationPromptController::class)
-    // ->name('verification.notice');
-    
-
-    // Route::get('verify-email/{id}/{hash}', VerifyEmailController::class)
-    // ->middleware(['signed', 'throttle:6,1'])
-    // ->name('verification.verify');
-    
-
-    // Route::post('email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
-    // ->middleware('throttle:6,1')
-    // ->name('verification.send');
-    
-
-    // Route::get('confirm-password', [ConfirmablePasswordController::class, 'show'])
-    // ->name('password.confirm');
-    
-
-    // Route::post('confirm-password', [ConfirmablePasswordController::class, 'store']);
-    
+        
 
     Route::put('password', [PasswordController::class, 'update'])->name('password.update');
     
