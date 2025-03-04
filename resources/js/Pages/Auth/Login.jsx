730d70d24778fcec77f46a9247815f01ae9a0a8f
franklinnot <franklinnot@outlook.com>
2025-03-03 21:24:29 -0500

    eliminacion de codigo de reset password en login

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -145,15 +145,6 @@ export default function Login({ status }) {
                     </div>
 
                     <div className="flex items-center justify-end mt-4">
-                        {/* {canResetPassword && (
-                        <Link
-                            href={route("password.request")}
-                            className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                        >
-                            Forgot your password?
-                        </Link>
-                    )} */}
-
                         <PrimaryButton
                             className="font-medium text-md w-full justify-center rounded-lg"
                             disabled={processing}
