6780c209f5dcc04ab48ad55037bea651a1043ac1
franklinnot <franklinnot@outlook.com>
2025-03-09 16:35:58 -0500

    modelo caja y sus metodos get y registrar

--- app/Models/Caja.php
+++ app/Models/Caja.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Caja extends Model
 {
@@ -22,6 +23,65 @@ class Caja extends Model
         'id_empresa'
 
     ];
+    #endregion
+
+    #region crud
+
+    public static function registrar(array $data): ?Caja
+    {
+        $result = DB::select(
+            "EXEC sp_registrar_caja 
+            @codigo = ?, @nombre = ?, @id_sucursal = ?, @id_empresa = ?",
+            [
+                $data['codigo'],
+                $data['nombre'],
+                $data['id_sucursal'],
+                $data['id_empresa'],
+            ]
+        );
+
+        return $result ? new Caja(['id_caja' => $result[0]->nuevo_id] + $data) : null;
+    }
+
+    public static function get_caja_by_id($id_caja): ?Caja
+    {
+        $result = DB::select("EXEC sp_get_caja_by_id @id_caja = ?", [$id_caja]);
+        return $result ? new Caja((array) $result[0]) : null;
+    }
+
+    // hace falta el store procedure
+    public static function get_cajas_by_id_sucursal($id_sucursal): ?array
+    {
+        $result = DB::select("EXEC sp_get_sucursales_by_id_empresa @id_sucursal = ?", [$id_sucursal]);
+        return $result ? array_map(function ($item) {
+            return new Sucursal((array) $item);
+        }, $result) : null;
+    }
+
+    // hace falta el store procedure
+    public static function get_cajas_by_id_empresa($id_empresa): ?array
+    {
+        $result = DB::select("EXEC sp_get_sucursales_by_id_empresa @id_empresa = ?", [$id_empresa]);
+        return $result ? array_map(function ($item) {
+            return new Sucursal((array) $item);
+        }, $result) : null;
+    }
+
+    public static function get_caja_by_codigo($codigo, $id_empresa): ?Caja
+    {
+        $result = DB::select("EXEC sp_get_caja_by_codigo @codigo = ?, @id_empresa = ?", [$codigo, $id_empresa]);
+        return $result ? new Caja((array) $result[0]) : null;
+    }
+
+    public static function existencia_caja_by_codigo($codigo, $id_empresa): ?bool
+    {
+        $result = DB::select("EXEC sp_existencia_caja_by_codigo @codigo = ?, @id_empresa = ?", [$codigo, $id_empresa]);
+        if (isset($result[0]->verificar)) {
+            return $result[0]->verificar === 'true';
+        }
+        return null;
+    }
+
     #endregion
 
 }
