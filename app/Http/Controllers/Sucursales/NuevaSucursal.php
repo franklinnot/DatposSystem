020a6d3151ac983fd630c6b5a778a6ba4975aff2
franklinnot <franklinnot@outlook.com>
2025-03-10 12:05:52 -0500

    eliminar almacen desde el sp al fallar el registro de sucursal

--- app/Http/Controllers/Sucursales/NuevaSucursal.php
+++ app/Http/Controllers/Sucursales/NuevaSucursal.php
@@ -77,8 +77,8 @@ class NuevaSucursal extends Controller
 
         // si no se registró correctamente la sucursal
         if (!$nueva_sucursal) {
-            // eliminamos el almacen que se registro
-            Almacen::eliminar_almacen_by_id($nuevo_almacen->id_almacen);
+            // eliminamos el almacen que se registro => ya no, pues el sp se encargara de ello
+            // Almacen::eliminar_almacen_by_id($nuevo_almacen->id_almacen);
             return $this->error();
         }
 
