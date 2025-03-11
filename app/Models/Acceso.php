5766d36e2b0b5bf1a1af03cd213ec5dbc4557779
franklinnot <franklinnot@outlook.com>
2025-03-10 21:53:23 -0500

    funcion get_accesos en el modelo Acceso

--- app/Models/Acceso.php
+++ app/Models/Acceso.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Acceso extends Model
 {
@@ -20,5 +21,13 @@ class Acceso extends Model
     ];
     #endregion
 
+
+    public static function get_accesos(): ?array
+    {
+        $result = DB::select("EXEC sp_get_accesos");
+        return $result ? array_map(function ($item) {
+            return new Acceso((array) $item);
+        }, $result) : null;
+    }
 
 }
