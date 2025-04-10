807dedd37ec84a903efb2232e66971c804886fa8
franklinnot <franklinnot@outlook.com>
2025-04-10 10:48:10 -0500

    refactor(auth): remove unused registration and password update routes
    The registration and password update routes were no longer used in the application, so they were removed to simplify the auth routes file and improve maintainability.

--- routes/auth.php
+++ routes/auth.php
@@ -28,18 +28,7 @@ Route::middleware('guest')->group(function () {
 
 Route::middleware(['auth', 'no.cache'])->group(function () {
 
-    Route::get('register', [RegisteredUserController::class, 'create'])
-                ->name('register');
-    
-
-    Route::post('register', [RegisteredUserController::class, 'store']);
-        
-
-    Route::put('password', [PasswordController::class, 'update'])->name('password.update');
-    
-
     Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
     ->name('logout');
-
 
 });
